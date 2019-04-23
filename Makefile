774304f68dfc8cd74f7d67fcf270134e6966f8b5
Todd Churchill <tchurchill@summitwireless.com>
2019-04-23 15:05:23 -0700

    Added AVAHI registration

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ endif
 
 ifeq ($(UNAME_S),Linux)
 CFLAGS +=  -I/usr//include/libusb-1.0
-LIBS += -lusb-1.0
+LIBS += -lusb-1.0 -L/usr/lib/x86_64-linux-gnu/ -lavahi-client -lavahi-common
 endif
 
 # uncomment this for SunOS
--- maws.c
+++ maws.c
@@ -8,15 +8,28 @@
         usage:  maws [-d] [-p port]
 */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>      /* needed for os x */
+#include <assert.h>
+#include <unistd.h>
 #include <string.h>      /* for memset */
 #include <sys/socket.h>
 #include <arpa/inet.h>   /* for printing an internet address in a user-friendly way */
 #include <netinet/in.h>
 #include <sys/errno.h>   /* defines ERESTART, EINTR */
 #include <sys/wait.h>    /* defines WNOHANG, for wait() */
-#include <unistd.h>
+#include <avahi-client/client.h>
+#include <avahi-client/publish.h>
+#include <avahi-common/alternative.h>
+#include <avahi-common/simple-watch.h>
+#include <avahi-common/malloc.h>
+#include <avahi-common/error.h>
+#include <avahi-common/timeval.h>
+
 #include <libusb.h>
 
 #include "port.h"       /* defines default port */
@@ -35,8 +48,16 @@
 
 extern int errno;
 
-void serve(int32_t port);   /* main server function */
+static AvahiEntryGroup *group = NULL;
+static AvahiSimplePoll *simple_poll = NULL;
+static char *name = NULL;
+static int32_t port = SERVICE_PORT;
 
+// Forward Declarations
+void serve(int32_t port);   /* main server function */
+static void create_services(AvahiClient *c);
+
+// Structures
 #pragma pack(1)
 typedef struct _netmsg_struct
 {
@@ -47,15 +68,135 @@ typedef struct _netmsg_struct
 } NET_MESSAGE;
 #pragma pack()
 
+static void entry_group_callback(AvahiEntryGroup *g, AvahiEntryGroupState state, AVAHI_GCC_UNUSED void *userdata) {
+    assert(g == group || group == NULL);
+    group = g;
+    /* Called whenever the entry group state changes */
+    switch (state) {
+        case AVAHI_ENTRY_GROUP_ESTABLISHED :
+            /* The entry group has been established successfully */
+            fprintf(stderr, "Service '%s' successfully established.\n", name);
+            break;
+        case AVAHI_ENTRY_GROUP_COLLISION : {
+            char *n;
+            /* A service name collision with a remote service
+             * happened. Let's pick a new name */
+            n = avahi_alternative_service_name(name);
+            avahi_free(name);
+            name = n;
+            fprintf(stderr, "Service name collision, renaming service to '%s'\n", name);
+            /* And recreate the services */
+            create_services(avahi_entry_group_get_client(g));
+            break;
+        }
+        case AVAHI_ENTRY_GROUP_FAILURE :
+            fprintf(stderr, "Entry group failure: %s\n", avahi_strerror(avahi_client_errno(avahi_entry_group_get_client(g))));
+            /* Some kind of failure happened while we were registering our services */
+            avahi_simple_poll_quit(simple_poll);
+            break;
+        case AVAHI_ENTRY_GROUP_UNCOMMITED:
+        case AVAHI_ENTRY_GROUP_REGISTERING:
+            ;
+    }
+}
+
+static void create_services(AvahiClient *c) {
+    char *n;
+    int ret;
+    assert(c);
+    /* If this is the first time we're called, let's create a new
+     * entry group if necessary */
+    if (!group)
+        if (!(group = avahi_entry_group_new(c, entry_group_callback, NULL))) {
+            fprintf(stderr, "avahi_entry_group_new() failed: %s\n", avahi_strerror(avahi_client_errno(c)));
+            goto fail;
+        }
+    /* If the group is empty (either because it was just created, or
+     * because it was reset previously, add our entries.  */
+    if (avahi_entry_group_is_empty(group)) {
+        fprintf(stderr, "Adding service '%s'\n", name);
+
+        /* Add the service for WISA */
+        if ((ret = avahi_entry_group_add_service(group, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC, 0, name, "_wisa._tcp", NULL, NULL, port, NULL, NULL, NULL)) < 0) {
+            if (ret == AVAHI_ERR_COLLISION)
+                goto collision;
+            fprintf(stderr, "Failed to add _wisa._tcp service: %s\n", avahi_strerror(ret));
+            goto fail;
+        }
+        /* Tell the server to register the service */
+        if ((ret = avahi_entry_group_commit(group)) < 0) {
+            fprintf(stderr, "Failed to commit entry group: %s\n", avahi_strerror(ret));
+            goto fail;
+        }
+    }
+    return;
+
+collision:
+    /* A service name collision with a local service happened. Let's
+     * pick a new name */
+    n = avahi_alternative_service_name(name);
+    avahi_free(name);
+    name = n;
+    fprintf(stderr, "Service name collision, renaming service to '%s'\n", name);
+    avahi_entry_group_reset(group);
+    create_services(c);
+    return;
+fail:
+    avahi_simple_poll_quit(simple_poll);
+}
+
+static void client_callback(AvahiClient *c, AvahiClientState state, AVAHI_GCC_UNUSED void * userdata) {
+    assert(c);
+    /* Called whenever the client or server state changes */
+    switch (state) {
+        case AVAHI_CLIENT_S_RUNNING:
+            /* The server has startup successfully and registered its host
+             * name on the network, so it's time to create our services */
+            create_services(c);
+            break;
+        case AVAHI_CLIENT_FAILURE:
+            fprintf(stderr, "Client failure: %s\n", avahi_strerror(avahi_client_errno(c)));
+            avahi_simple_poll_quit(simple_poll);
+            break;
+        case AVAHI_CLIENT_S_COLLISION:
+            /* Let's drop our registered services. When the server is back
+             * in AVAHI_SERVER_RUNNING state we will register them
+             * again with the new host name. */
+        case AVAHI_CLIENT_S_REGISTERING:
+            /* The server records are now being established. This
+             * might be caused by a host name change. We need to wait
+             * for our own records to register until the host name is
+             * properly esatblished. */
+            if (group)
+                avahi_entry_group_reset(group);
+            break;
+        case AVAHI_CLIENT_CONNECTING:
+            ;
+    }
+}
+
+static void server_callback(AVAHI_GCC_UNUSED AvahiTimeout *e, void *userdata) {
+    AvahiClient *client = userdata;
+
+    // start server
+    serve(port);
+}
+
 int main(int argc, char **argv)
 {
     extern char *optarg;
     extern int optind;
     int32_t c, err = 0; 
-    int32_t port = SERVICE_PORT;
+    uint8_t *room = NULL;
+
+    AvahiClient *client = NULL;
+    int error;
+    int ret = 1;
+    struct timeval tv;
+
     static char usage[] = "usage: %s [-d] [-p port]\n";
 
-    while ((c = getopt(argc, argv, "dp:")) != -1) {
+    while ((c = getopt(argc, argv, "dpn:")) != -1) {
        switch (c) {
            case 'p':
                port = atoi(optarg);
@@ -63,6 +204,9 @@ int main(int argc, char **argv)
                    fprintf(stderr, "invalid port number: %s\n", optarg);
                    err = 1;
                }
+               break;
+           case 'n':
+	       room = optarg;
                break;
            case '?':
                err = 1;
@@ -74,7 +218,47 @@ int main(int argc, char **argv)
             fprintf(stderr, usage, argv[0]);
             exit(1);
     }
-    serve(port);
+
+    // ------------------------------------------------------
+    // Setup Avahai service
+    // ------------------------------------------------------
+    /* Allocate main loop object */
+    if (!(simple_poll = avahi_simple_poll_new())) {
+        fprintf(stderr, "Failed to create simple poll object.\n");
+        goto fail;
+    }
+
+    name = avahi_strdup(room);
+
+    /* Allocate a new client */
+    client = avahi_client_new(avahi_simple_poll_get(simple_poll), 0, client_callback, NULL, &error);
+
+    /* Check whether creating the client object succeeded */
+    if (!client) {
+        fprintf(stderr, "Failed to create client: %s\n", avahi_strerror(error));
+        goto fail;
+    }
+
+    // After 5s ... start server
+    avahi_simple_poll_get(simple_poll)->timeout_new(
+        avahi_simple_poll_get(simple_poll),
+        avahi_elapse_time(&tv, 1000*5, 0),
+        server_callback,
+        client);
+
+    /* Run the main loop */
+    avahi_simple_poll_loop(simple_poll);
+    ret = 0;
+
+
+fail:
+    /* Cleanup things */
+    if (client)
+        avahi_client_free(client);
+    if (simple_poll)
+        avahi_simple_poll_free(simple_poll);
+    avahi_free(name);
+    return ret;
 }
 
 /* serve: set up the service */
